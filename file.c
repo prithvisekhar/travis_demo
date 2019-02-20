ae4dc3bc8992548d0a9ae84bd1b4a6608652eb49
AshwiniK10 <35912753+AshwiniK10@users.noreply.github.com>
2019-02-20 10:27:53 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,15 +1,19 @@
+
 #include <stdio.h>
 int main()
 {
-    int i, j, rows=5;
-    printf("Enter number of rows: ");
-    for(i=1; i<=rows; ++i)
+    int n=10, i;
+    unsigned long long factorial = 1;
+    printf("Enter an integer: ");
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else
     {
-        for(j=1; j<=i; ++j)
+        for(i=1; i<=n; ++i)
         {
-            printf("* ");
+            factorial *= i;            
         }
-        printf("\n");
+        printf("Factorial of %d = %llu", n, factorial);
     }
     return 0;
 }
